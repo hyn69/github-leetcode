71045cc2a7d2b47782f9e7210157098f950d1d2e
hyn69 <hoangamerminecraft@gmail.com>
2024-03-06 10:42:00 +0700

    Add LeetCode

---
+++ LeetCode.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* next;
+}
+int getLength(Node* pHead) {
+    Node* pCur = pHead;
+    int count = 0;
+    while (pCur) {
+        count++;
+        pCur = pCur -> next;
+    }
+    return count;
+}
+Node* merge(Node* l1, Node* l2) {
+    
+}
+Node* middleNode(Node* l1) {
+    Node* t1 = l1;
+    Node* t2 = l2;
+    while (t1 != NULL && t1 -> next != NULL) {
+        t2 = t2 -> next;
+        t1 = t1 -> next -> next;
+    }
+    return t2;
+}
+int main() {
+    
+}
